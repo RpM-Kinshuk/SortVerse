92c754f3d1211c3c945182115bc9665330eb4ba3
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2022-09-04 17:07:16 +0530

    Added Hoare Partition

--- Sorting in C++/Quick Sort.hpp
+++ Sorting in C++/Quick Sort.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 using std::vector;
+
 namespace tmp
 {
     long long Split(vector<long long> &arr, long long l, long long r)
@@ -18,16 +19,83 @@ namespace tmp
         std::swap(arr[i + 1], arr[r]);
         return i + 1;
     }
+
+    void InsSort(vector<long long> &arr, long long l, long long r)
+    {
+        for (long long i = l + 1; i <= r; i++)
+        {
+            long long curr = arr[i];
+            long long j = i;
+            while (j > l && arr[j - 1] > curr)
+            {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = curr;
+        }
+    }
+
+    long long Hoarepartition(vector<long long> &a, long long low, long long high)
+    {
+        long long pivot = a[low];
+        long long i = low - 1;
+        long long j = high + 1;
+        while (true)
+        {
+            do
+            {
+                i++;
+            } while (a[i] < pivot);
+
+            do
+            {
+                j--;
+            } while (a[j] > pivot);
+
+            if (i >= j)
+            {
+                return j;
+            }
+            std::swap(a[i], a[j]);
+        }
+    }
 }
+
 namespace Ki
 {
     void QuickSort(vector<long long> &arr, long long l, long long r)
     {
         if (l < r)
         {
-            long long pi = tmp::Split(arr, l, r);
+            long long pi = tmp::Hoarepartition(arr, l, r);
             QuickSort(arr, pi + 1, r);
             QuickSort(arr, l, pi - 1);
+        }
+    }
+
+    void HybridQuickSort(vector<long long> &arr, long long l, long long r)
+    {
+        while (l < r)
+        {
+            if (r - l + 1 < 10)
+            {
+                tmp::InsSort(arr, l, r);
+                break;
+            }
+            else
+            {
+                long long pi = tmp::Hoarepartition(arr, l, r);
+                if (pi - l < r - pi)
+                {
+                    HybridQuickSort(arr, l, pi - 1);
+                    l = pi + 1;
+                }
+                else
+                {
+                    HybridQuickSort(arr, pi + 1, r);
+                    r = pi - 1;
+                }
+            }
         }
     }
 }
