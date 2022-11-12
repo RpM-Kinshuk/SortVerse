ba26526b7cd5e2f728d46363cc813a34d8504af7
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2022-11-13 04:05:11 +0530

    Added C Handler

---
+++ C Handler.hpp
@@ -0,0 +1,37 @@
+#include "Sorting in C/c_wrap.hpp"
+#include <iostream>
+#include <chrono>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::vector;
+
+#define ClockStart t1 = std::chrono::high_resolution_clock::now();
+#define ClockEnd t2 = std::chrono::high_resolution_clock::now();
+#define FindDuration std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()
+
+namespace Ki
+{
+    void CSort(vector <long long> vec, long long n)
+    {
+        cout << "\n\n";
+        cout << "~~~~~~~~~~~~~ Sorting with C ~~~~~~~~~~~~~\n\n";
+        long long *temp = tmp::vectorray(vec, n);
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+        c_bubble(temp, n);
+        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+        cout << "> Bubble Sort: " << duration << " microseconds\n\n";
+        free(temp); temp = tmp::vectorray(vec, n);
+
+        ClockStart
+            c_selectsrt(temp, n);
+        ClockEnd
+            duration = FindDuration;
+        cout << "> Selection Sort: " << duration << " microseconds\n\n";
+        free(temp); temp = tmp::vectorray(vec, n);
+
+        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
+    }
+}
