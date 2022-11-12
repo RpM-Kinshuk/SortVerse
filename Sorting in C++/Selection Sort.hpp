fa4edf65a3ecf24418b19fc74a0779f3fc245adf
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2022-11-13 03:27:51 +0530

    Improved C++ Selection Sort

--- Sorting in C++/Selection Sort.hpp
+++ Sorting in C++/Selection Sort.hpp
@@ -6,13 +6,15 @@ namespace Ki
     {
         for (long long i = 0; i < n - 1; i++)
         {
+            long long min_idx = i;
             for (long long j = i + 1; j < n; j++)
             {
-                if (arr[j] < arr[i])
+                if (arr[j] < arr[min_idx])
                 {
-                    std::swap(arr[i], arr[j]);
+                    min_idx = j;
                 }
             }
+            std::swap(arr[min_idx], arr[i]);
         }
     }
 }
