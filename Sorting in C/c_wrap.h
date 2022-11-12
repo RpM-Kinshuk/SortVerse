ac030af9be521b7fcf40370aa6d3abab30853e0c
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2022-11-13 03:25:55 +0530

    Added C Wrapper

---
+++ Sorting in C/c_wrap.h
@@ -0,0 +1,38 @@
+#define customswp
+#include "swp.h"
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <float.h>
+#include <ctype.h>
+#include <errno.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <time.h>
+#include "BubbleSort.h"
+#include "InsertionSort.h"
+#include "SelectionSort.h"
+#include "MergeSort.h"
+#include "QuickSort.h"
+#include "DNFSort.h"
+#include "CountSort.h"
+#include "WaveSort.h"
+*/
+
+
+
