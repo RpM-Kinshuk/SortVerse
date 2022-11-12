06f7b49bcde0d8da144908c80aab504c9d35804d
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2022-11-13 04:03:20 +0530

    Changed C wrapper to hpp

---
+++ Sorting in C/c_wrap.hpp
@@ -0,0 +1,37 @@
+#define customswp
+#include "swp.h"
+#include "vectorray.hpp"
+#include "c_bubble.h"
+#include "c_selsrt.h"
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <float.h>
+#include <ctype.h>
+#include <errno.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <time.h>
+#include "InsertionSort.h"
+#include "MergeSort.h"
+#include "QuickSort.h"
+#include "DNFSort.h"
+#include "CountSort.h"
+#include "WaveSort.h"
+*/
+
+
+
