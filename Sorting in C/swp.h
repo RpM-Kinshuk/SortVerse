15091fe042e528c5d31c4204c8e86f6f7495dc3b
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2022-11-13 03:25:06 +0530

    Improved C Swap

--- Sorting in C/swp.h
+++ Sorting in C/swp.h
@@ -1,11 +1,20 @@
-void swap(long long *a, long long *b){
-    asm("" : "=r" (*a), "=r" (*b) : "1" (*a), "0" (*b));
+void swap(long long *a, long long *b)
+{
+    asm(""
+        : "=r"(*a), "=r"(*b)
+        : "1"(*a), "0"(*b));
 }
 
-void swap(int *a, int *b){
-    asm("" : "=r" (*a), "=r" (*b) : "1" (*a), "0" (*b));
+void swap(int *a, int *b)
+{
+    asm(""
+        : "=r"(*a), "=r"(*b)
+        : "1"(*a), "0"(*b));
 }
 
-void swap(char *a, char *b){
-    asm("" : "=r" (*a), "=r" (*b) : "1" (*a), "0" (*b));
+void swap(char *a, char *b)
+{
+    asm(""
+        : "=r"(*a), "=r"(*b)
+        : "1"(*a), "0"(*b));
 }
