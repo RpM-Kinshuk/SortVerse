03bfef9d0a5b699927811ed8e834b6391484a143
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2023-01-12 00:17:24 +0530

    Fixed some bug

--- Java_Handler.java
+++ Java_Handler.java
@@ -11,16 +11,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Java_Handler {
-    void bubbleSort(int arr[], int n) {
-        for (int i = 0; i < n - 1; i++)
-            for (int j = 0; j < n - i - 1; j++)
-                if (arr[j] > arr[j + 1]) {
-                    // swap arr[j+1] and arr[j]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+    void bubbleSort(int arr[],int n)
+    {
+        for(int i=0;i<n-1;i++)
+        {
+            boolean flag=false;
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    flag=true;
                 }
-
+            }
+            if(flag==false)
+                break;
+        }
     }
 
     void countSort(int arr[], int n) {
