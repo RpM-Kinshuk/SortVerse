c50bfda114db338186e66fa0e09b6f6e32845546
Palak Dwivedi <98268910+PalakDwivedi02@users.noreply.github.com>
2023-02-09 01:15:34 +0530

    Added Hybrid Quick Sort

--- Java_Handler.java
+++ Java_Handler.java
@@ -140,6 +140,19 @@ public class Java_Handler {
         a[end] = t;
         return (i + 1);
     }
+    
+     public void insSort(int arr[], int l, int r) {
+        int i, j;
+        for (i = l + 1; i <= r; i++) {
+            int curr = arr[i];
+            j = i;
+            while (j > l && curr < arr[j-1]) {
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = curr;
+        }
+    }
 
     void quickSort(int a[], int start, int end) {
         if (start < end) {
@@ -147,7 +160,48 @@ public class Java_Handler {
             quickSort(a, start, p - 1);
             quickSort(a, p + 1, end);
         }
+    }
+    
+     public int Hoarepartition(int a[], int low, int high) {
+        int pivot = a[low];
+        int i = low - 1;
+        int j = high + 1;
+        while (true) {
+            do {
+                i++;
+            } while (a[i] < pivot);
 
+            do {
+                j--;
+            } while (a[j] > pivot);
+
+            if (i >= j) {
+                return j;
+            }
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+    
+    public void HybridQuickSort(int arr[], int l, int r) {
+        while (l < r) {
+            if (r - l + 1 < 10) {
+                insSort(arr, l, r);
+                break;
+            } 
+            else {
+                int pi = Hoarepartition(arr, l, r);
+                if (pi - l < r - pi) {
+                    HybridQuickSort(arr, l, pi - 1);
+                    l = pi + 1;
+                }
+                else {
+                    HybridQuickSort(arr, pi + 1, r);
+                    r = pi - 1;
+                }
+            }
+        }
     }
 
     void selectionSort(int arr[], int n) {
