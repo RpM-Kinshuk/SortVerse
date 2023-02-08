0d249e5945a64718b42015a19f1124c1a77fbbe1
Palak Dwivedi <98268910+PalakDwivedi02@users.noreply.github.com>
2023-02-09 01:05:32 +0530

    Updated Quick Sort

--- Sorting_in_Java/Java code/Quick.java
+++ Sorting_in_Java/Java code/Quick.java
@@ -1,10 +1,8 @@
 package Sorting_in_Java;
-public class Quick
-{
+public class Quick {
     public int partition(int a[], int start, int end) {
         int pivot = a[end];
         int i = (start - 1);
-
         for (int j = start; j <= end - 1; j++) {
             if (a[j] < pivot) {
                 i++;
@@ -19,6 +17,19 @@ public class Quick
         return (i + 1);
     }
 
+    public void insSort(int arr[], int l, int r) {
+        int i, j;
+        for (i = l + 1; i <= r; i++) {
+            int curr = arr[i];
+            j = i;
+            while (j > l && curr < arr[j-1]) {
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = curr;
+        }
+    }
+
     public void quicksort(int a[], int start, int end) {
         if (start < end) {
             int p = partition(a, start, end);
@@ -27,10 +38,46 @@ public class Quick
         }
 
     }
-} 
-/*void printArr(long a[], long n)  
-{  
-    long i;  
-    for (i = 0; i < n; i++)  
-        System.out.print(a[i] + " ");  
-} */
+
+    public int Hoarepartition(int a[], int low, int high) {
+        int pivot = a[low];
+        int i = low - 1;
+        int j = high + 1;
+        while (true) {
+            do {
+                i++;
+            } while (a[i] < pivot);
+
+            do {
+                j--;
+            } while (a[j] > pivot);
+
+            if (i >= j) {
+                return j;
+            }
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+
+    public void HybridQuickSort(int arr[], int l, int r) {
+        while (l < r) {
+            if (r - l + 1 < 10) {
+                insSort(arr, l, r);
+                break;
+            } 
+            else {
+                int pi = Hoarepartition(arr, l, r);
+                if (pi - l < r - pi) {
+                    HybridQuickSort(arr, l, pi - 1);
+                    l = pi + 1;
+                }
+                else {
+                    HybridQuickSort(arr, pi + 1, r);
+                    r = pi - 1;
+                }
+            }
+        }
+    }
+}
