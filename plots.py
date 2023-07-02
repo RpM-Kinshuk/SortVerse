40f84a1415641f3fcb47230ed7fa8f4b7fc59b53
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2023-07-03 03:03:07 +0530

    Added Support for images in the plots

--- plots.py
+++ plots.py
@@ -1,55 +1,58 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+import os
 
+# os.chdir(os.path.dirname(os.path.abspath(__file__))) 
+# to change working directory to the current file's directory
+# Config
+show_plots = False
+dpi = 1200
+remove_orig_img = True
+
+# Plotting
 plt.rcParams["figure.figsize"] = [10.00, 7.00]
 plt.rcParams["figure.autolayout"] = True
 x = [10, 100, 1000, 10000, 100000, 1000000]
 x_ticks = range(len(x))
 
-def xticks():
+def xticks_ylim():
     plt.xticks(x_ticks, x)
-
-def ylm():
     plt.ylim(5, 5e4)
 
-#Plot for Java
+def plt_and_save():
+    plt.title(title)
+    plt.xlabel("No. of elements")
+    plt.ylabel("Time Taken")
+    plt.legend(loc='upper left')
+    plt.savefig(f'{title}.png', dpi=dpi)
+    if show_plots:
+        plt.show()
 
-y1 = np.array([482, 358, 970,5202, 46152,116218])
-y2 = np.array([4,58,2046,15753,754144,74976328])
-y3 = np.array([63,8,691,2868,20751,160941])
-y4 = np.array([34,5,431,1729,13495,80779])
-y5 = np.array([116,3,2530,141581,11672102,516213217])
-y6 = np.array([992,999,1054,2315,10383,33331])
-y7 = np.array([179,5,4853,103615,16243540,1577919139])
-y8 = np.array([25,6,316,3130,16448,69536])
-y9 = np.array([74,7,682,4432,28327,163092])
-y = np.array([53,15,638,3489,18437,107633])
-y0 = np.array([31,5,510,8305,15634,145878])
+# Combine images
+def combine():
+    c = Image.open('C.png')
+    cplus = Image.open('C++.png')
+    java = Image.open('Java.png')
+    python = Image.open('Python.png')
+    width, height = cplus.size # Assuming all images have the same dimensions
+    collage_width = width * 2
+    collage_height = height * 2
+    collage = Image.new('RGB', (collage_width, collage_height))
+    collage.paste(c, (0, 0))
+    collage.paste(cplus, (width, 0))
+    collage.paste(java, (0, height))
+    collage.paste(python, (width, height))
+    collage.save('Comparison.png')
+    if remove_orig_img:
+        os.remove('C.png')
+        os.remove('C++.png')
+        os.remove('Java.png')
+        os.remove('Python.png')
 
-xticks()
-ylm()
-
-plt.plot(y,color='black',label='Radix')
-plt.plot(y0,color='blue',label='Shell')
-plt.plot(y1,color='green',label='Inbuilt')
-plt.plot(y2,color='orange',label='Insertion')
-plt.plot(y3,color='red',label='Merge')
-plt.plot(y4,color='pink',label='Quick')
-plt.plot(y5,color='brown',label='Selection')
-plt.plot(y6,color='magenta',label='Counting')
-plt.plot(y7,color='yellow',label='Bubble')
-plt.plot(y8,color='cyan',label='Hybrid Quick')
-plt.plot(y9,color='grey',label='Heap')
-
-plt.title("Java")
-plt.xlabel("No. of elements")
-plt.ylabel("Time Taken")
-
-plt.legend()
-plt.show()
 
 #Plot for C
-
+title = "C"
 y1 = np.array([5,10,86,878,10602,91497])
 y2 = np.array([6,10,577,50835,5154775,191199687])
 y3 = np.array([2,8,106,1114,14507,93086])
@@ -62,8 +65,7 @@ y9 = np.array([1,9,105,1669,17796,252654])
 y = np.array([2,8,75,784,7922,54762])
 y0 = np.array([0,5,85,1480,19107,124359])
 
-xticks()
-ylm()
+xticks_ylim()
 
 plt.plot(y,color='black',label='Radix')
 plt.plot(y0,color='blue',label='Shell')
@@ -77,15 +79,11 @@ plt.plot(y7,color='yellow',label='Bubble')
 plt.plot(y8,color='cyan',label='Hybrid Quick')
 plt.plot(y9,color='grey',label='Heap')
 
-plt.title("C")
-plt.xlabel("No. of elements")
-plt.ylabel("Time Taken")
+plt_and_save()
 
-plt.legend()
-plt.show()
 
 #Plot for C++
-
+title = "C++"
 y1 = np.array([2,15,143,2829,22399,53231])
 y2 = np.array([1,14,1165,116934,11559938,183391337])
 y3 = np.array([1,7,87,1235,15328,70644])
@@ -98,8 +96,7 @@ y9 = np.array([1,17,194,3280,34610,178613])
 y = np.array([4,15,115,1130,11069,64414])
 y0 = np.array([0,10,136,2572,32167,128752])
 
-xticks()
-ylm()
+xticks_ylim()
 
 plt.plot(y,color='black',label='Radix')
 plt.plot(y0,color='blue',label='Shell')
@@ -113,15 +110,42 @@ plt.plot(y7,color='yellow',label='Bubble')
 plt.plot(y8,color='cyan',label='Hybrid Quick')
 plt.plot(y9,color='grey',label='Heap')
 
-plt.title("C++")
-plt.xlabel("No. of elements")
-plt.ylabel("Time Taken")
+plt_and_save()
 
-plt.legend()
-plt.show()
+
+#Plot for Java
+title = "Java"
+y1 = np.array([482, 358, 970,5202, 46152,116218])
+y2 = np.array([4,58,2046,15753,754144,74976328])
+y3 = np.array([63,8,691,2868,20751,160941])
+y4 = np.array([34,5,431,1729,13495,80779])
+y5 = np.array([116,3,2530,141581,11672102,516213217])
+y6 = np.array([992,999,1054,2315,10383,33331])
+y7 = np.array([179,5,4853,103615,16243540,1577919139])
+y8 = np.array([25,6,316,3130,16448,69536])
+y9 = np.array([74,7,682,4432,28327,163092])
+y = np.array([53,15,638,3489,18437,107633])
+y0 = np.array([31,5,510,8305,15634,145878])
+
+xticks_ylim()
+
+plt.plot(y,color='black',label='Radix')
+plt.plot(y0,color='blue',label='Shell')
+plt.plot(y1,color='green',label='Inbuilt')
+plt.plot(y2,color='orange',label='Insertion')
+plt.plot(y3,color='red',label='Merge')
+plt.plot(y4,color='pink',label='Quick')
+plt.plot(y5,color='brown',label='Selection')
+plt.plot(y6,color='magenta',label='Counting')
+plt.plot(y7,color='yellow',label='Bubble')
+plt.plot(y8,color='cyan',label='Hybrid Quick')
+plt.plot(y9,color='grey',label='Heap')
+
+plt_and_save()
+
 
 #Plot for Python
-
+title = "Python"
 y1 = np.array([2,7,79,1030,15645,242196])
 y2 = np.array([8,217,31071,3020276,46458039,52277799787])
 y3 = np.array([28,207,2780,34131,450521,5392594])
@@ -134,8 +158,7 @@ y9 = np.array([17,199,3106,46508,664555,8108707])
 y = np.array([35,189,1797,18848,275071,3002543])
 y0 = np.array([7,343,43556,4390244,78388133,80671826754])
 
-xticks()
-ylm()
+xticks_ylim()
 
 plt.plot(y,color='black',label='Radix')
 plt.plot(y0,color='blue',label='Shell')
@@ -149,9 +172,7 @@ plt.plot(y7,color='yellow',label='Bubble')
 plt.plot(y8,color='cyan',label='Hybrid Quick')
 plt.plot(y9,color='grey',label='Heap')
 
-plt.title("Python")
-plt.xlabel("No. of elements")
-plt.ylabel("Time Taken")
+plt_and_save()
 
-plt.legend()
-plt.show()
+
+combine()
